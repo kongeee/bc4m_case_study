43d9ea540a09a1855ac5b1ad13d452dc4745cdc7
kongeee <furkanekici1997@gmail.com>
2022-02-01 21:01:55 +0300

    first commit

---
+++ BCFM/Controllers/BCFMController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Json.Net;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+
+namespace BCFM.Controllers {
+    [Route("")]
+    [ApiController]
+    public class BCFMController : ControllerBase {
+
+        private IConfiguration _configuration;
+        public BCFMController(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+
+        [HttpGet("")]
+        public IActionResult GetName() {
+            Person person1 = new Person { Name = "Furkan", Surname = "Ekici" };
+            return Ok(person1);
+        }
+       
+        
+        [HttpPost]
+        [Route("")]
+        [Route("temperature")]
+        public IActionResult ReturnErrorMessage() {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("message", "The method is not allowed for the requested URL.");
+
+            return BadRequest(dict);
+        }
+
+
+
+        [HttpGet("temperature")]
+        public IActionResult GetTemperature(string city) {
+           
+            var apiKey = _configuration.GetSection("APIKey").Value;
+            var url = "http://api.weatherapi.com/v1/current.json?q=" + city + "&key=" + apiKey;
+            var request = WebRequest.Create(url);
+            request.Method = "GET";
+
+            using var webResponse = request.GetResponse();
+            using var webStream = webResponse.GetResponseStream();
+
+            using var reader = new StreamReader(webStream);
+            var data = reader.ReadToEnd();
+            dynamic innerData = JObject.Parse(data);
+            int temperature = innerData.current.temp_c;
+            City city1 = new City { CityName = city.ToLower(), Temperature = temperature };
+            
+            return Ok(city1);
+        }
+    }
+
+    class City {
+        public string CityName { get; set; }
+        public double Temperature { get; set; }
+    }
+
+    class Person {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
