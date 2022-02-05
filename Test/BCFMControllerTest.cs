f438c048841db51502e2d787b8639a80bdb3eb84
kongeee <furkanekici1997@gmail.com>
2022-02-05 21:54:24 +0300

    05.02.2022 update

--- Test/BCFMControllerTest.cs
+++ Test/BCFMControllerTest.cs
@@ -1,14 +1,18 @@
 using BCFM.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Net;
 using Xunit;
 
 namespace Test2 {
     public class BCFMControllerTest {
         [Fact]
-        public void TemperatureEnPointShouldReturnOk() {
-           
-            Assert.Equal(1, 1);
+        public void DefaultEndPointShouldReturnOk() {
+            string url = "http://localhost:44320";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Assert.Equal(response.StatusCode, HttpStatusCode.OK);
         }
     }
 }
